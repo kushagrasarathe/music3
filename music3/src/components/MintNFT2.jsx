657a868166038cbf9fa5c7a1c306c5734d1034bc
dhruv <dhruvagarwal2017@gmail.com>
2022-07-23 21:04:51 +0530

    NFTport NFT minting

--- music3/src/components/MintNFT2.jsx
+++ music3/src/components/MintNFT2.jsx
@@ -1,5 +1,28 @@
 import React from "react";
-
-export const MintNFT2 = () => {
+import { useEffect, useState } from "react";
+export const MintNFT2 = ({ ipfsURI, userAddress }) => {
+  const [response, SetResponse] = useState();
+  const mintNFT = () => {
+    fetch("https://api.nftport.xyz/v0/mints/customizable", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: process.env.NFTPORT_API_KEY,
+      },
+      body: {
+        chain: "polygon",
+        contract_address: "0xDBd95D79d3A019Cd16D02B6a386aDa8C717C6A09",
+        metadata_uri: ipfsURI,
+        mint_to_address: userAddress,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        SetResponse(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return <div>MintNFT2</div>;
 };
