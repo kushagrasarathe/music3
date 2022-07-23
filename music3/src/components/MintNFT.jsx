b4b297d0ef78cf4b1504a79d28fff6b5ff570982
dhruv <dhruvagarwal2017@gmail.com>
2022-07-23 19:28:32 +0530

    NFt port

--- music3/src/components/MintNFT.jsx
+++ music3/src/components/MintNFT.jsx
@@ -1,5 +1,25 @@
-import React from "react";
-
-export const MintNFT = () => {
+export const MintNFT = ({ Name, Description, ipfsURL, userAddress }) => {
+  const mintNFT = async ({ Name, Description, ipfsURL, userAddress }) => {
+    fetch("https://api.nftport.xyz/v0/mints/easy/urls", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: process.env.NFTPORT_API_KEY,
+      },
+      body: {
+        chain: "polygon",
+        name: Name,
+        description: Description,
+        file_url: ipfsURL,
+        mint_to_address: userAddress,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return <div>MintNFT</div>;
 };
