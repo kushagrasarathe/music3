7f1d1694c2a4d797906ae656bb101ffa6abc518c
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-07-23 18:34:20 +0530

    added footer in layout

--- music3/src/components/Layout.jsx
+++ music3/src/components/Layout.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Layout({ children }) {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -12,8 +13,26 @@ export default function Layout({ children }) {
   return (
     <>
       <header>
-
+        <nav className={styles.navbar}>
+            <span><Link href={"/"} >music3</Link></span>
+        </nav>
       </header>
+
+      { children }
+      {/* footer */}
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
     </>
   );
 }
