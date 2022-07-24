9f4e58393d87da63814eb24aa9eeba2a43ac2a10
dhruv <dhruvagarwal2017@gmail.com>
2022-07-24 19:11:07 +0530

    pkg update

--- music3/pages/_app.js
+++ music3/pages/_app.js
@@ -1,27 +1,6 @@
 import "../styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
 import Layout from "../src/components/Layout";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
 import "../styles/globals.css";
-
-const { chains, provider } = configureChains(
-  [chain.polygon],
-  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "Music 3",
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
 
 function MyApp({ Component, pageProps }) {
   return (
