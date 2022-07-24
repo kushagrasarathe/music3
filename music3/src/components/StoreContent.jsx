c3646e40bce3053df9d243a5d3a44008f4bb7ea2
dhruv <dhruvagarwal2017@gmail.com>
2022-07-24 19:02:12 +0530

    error resolved

--- music3/src/components/StoreContent.jsx
+++ music3/src/components/StoreContent.jsx
@@ -3,17 +3,17 @@ import { Web3Storage } from "web3.storage";
 //// used Web3.storage to upload music file to IPFS
 
 const web3storage_key= process.env.WEB3STORAGE_TOKEN;
-function getAccessToken() {
+function GetAccessToken() {
   return web3storage_key;
 }
 
-function makeStorageClient() {
-  return new Web3Storage({ token: getAccessToken() });
+function MakeStorageClient() {
+  return new Web3Storage({ token: GetAccessToken() });
 }
 
 export const StoreContent = async (files) => {
   console.log("Uploading Audio to IPFS with web3.storage....");
-  const client = makeStorageClient();
+  const client = MakeStorageClient();
   const cid = await client.put([files]);
   console.log("stored files with cid:", cid);
   console.log("Song Uploaded ton IPFS successfully 🚀🚀");
